9ef5764fe4c54c9af66a75b6f6a86e0327dee92c
FABIO JUNIOR RIBEIRO <37555641+frib3iro@users.noreply.github.com>
2021-03-27 16:27:18 -0300

    Add files via upload

--- github.sh
+++ github.sh
@@ -15,16 +15,13 @@ if [ -e GitHub ]
 then
    echo -e "${S} ${Y}O diretório GitHub existe${F}"
    echo
-   sleep 2
    cd GitHub
 else
    echo -e "${S} ${Y}O diretório GitHub não existe e será criado${F}"
    echo
-   sleep 2
    mkdir GitHub && cd GitHub
    echo -e "${S} ${G}Repositório GitHub criado com sucesso!${F}"
    echo
-   sleep 2
 fi
 
 for i in ${lista[@]}
@@ -33,14 +30,12 @@ do
    then
         echo -e "${S} ${Y}O repositório $i existe${F}"
         echo
-        sleep 2
     else
         echo -e "${S} ${Y}Clonando o repositório $i ${F}"
-        sleep 2
         git clone https://github.com/frib3iro/$i
         echo -e "${S} ${G}Repositório $i clonado com sucesso!!${F}"
         echo
-        sleep 2
+        
     fi
 done
 
