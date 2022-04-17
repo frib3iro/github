e68cb3ef2ffec057958e8bae8ac8af40a9673bae
Fabio Junior Ribeiro <rib3iro@live.com>
2022-04-17 12:04:45 -0300

    Tudo ok

--- github.sh
+++ github.sh
@@ -15,7 +15,7 @@ F='\e[m'
 LOG=erro.txt
 # ------------------------------------------------------
 
-lista=(arch archlinux archlinuxMBR bspwm cinnamon curso-java-basico debian github gnome grub i3wm Java JavaGame_DoomFire MegaSena plymouth Scripts Snake ssh vbz zsh)
+lista=(arch archlinux archlinuxMBR bspwm cinnamon curso-java-basico debian github grub i3wm Java JavaGame_DoomFire MegaSena plymouth Scripts Snake ssh vbz zsh)
 
 clear && cd /home/fabio
 
